bbf808688f43aaa7caf0265f13d791941f91284a
D-Se <2869830202@qq.com>
2022-07-10 13:01:31 +0800

    raw unit test

--- inst/tinytest/test_hash.R
+++ inst/tinytest/test_hash.R
@@ -1,4 +1,7 @@
 # suppressMessages(library(dav))
+data <- charToRaw("a test string")
 
-expect_equal(dav:::hash(mtcars), "4e53d263399fe064")
-expect_equal(dav:::hash(mtcars, "128"), "d2264969c3aae7044e53d263399fe064")
+# expect_equal(dav:::hash(data, "xxh3"), "4e53d263399fe064")
+# expect_equal(dav:::hash(data, "128"), "d2264969c3aae7044e53d263399fe064")
+expect_equal(dav:::hash(data, "xxh3"), "1bcf3089d866d150")
+expect_equal(dav:::hash(data, "128"), "ed039fa8fe316fab4a0b7ef411ea9c04")
