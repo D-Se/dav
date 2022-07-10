3bd881eb7c1e1f9901511539c177e28eceb74519
D-Se <2869830202@qq.com>
2022-07-10 11:23:14 +0800

    adjust github workflow

--- tests/testthat/test-hash.R
+++ tests/testthat/test-hash.R
@@ -1,4 +1,4 @@
 test_that("xxhash output is known", {
   expect_equal(hash(mtcars), "4e53d263399fe064")
-  expect_equal(hash(mtcars), "d2264969c3aae7044e53d263399fe064")
+  expect_equal(hash(mtcars, "128"), "d2264969c3aae7044e53d263399fe064")
 })
